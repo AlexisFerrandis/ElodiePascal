f4d118d630b4bf3997e17a0cddd143e9aa981773
Alexis Ferrandis <alexis.1.ferrandis@gmail.com>
2022-08-10 17:44:40 +0200

    Reset

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+
+import Home from "./components/sections/Home";
+
+const App = () => {
+	return (
+		<div className="App">
+			<NavBar />
+			<Home />
+		</div>
+	);
+};
+
+export default App;
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavBar = () => {
+	return (
+		<nav>
+			<ul>
+				<li>NAVBAR</li>
+			</ul>
+		</nav>
+	);
+};
+
+export default NavBar;
